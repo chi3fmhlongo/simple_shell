09a8f32b46b6aab2a6f42823f4168946c6400ba8
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-11-17 10:30:19 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,14 +1,5 @@
 #include "shell.h"
 
-/**
- * tokenize - parsing user input into arguments
- *            by splits an array string into tokens using a delimiter.
- * @str: the string to be tokenized.
- * @delim: the delimiter used to split the string.
- *
- * Return: an array of pointers to the tokens,
- *         or NULL if an error occurs.
- */
 char **tokenize(char *str, const char *delim)
 {
 	char *token = NULL;
@@ -30,7 +21,7 @@ char **tokenize(char *str, const char *delim)
 		token = strtok(NULL, delim);
 		i++;
 	}
-	/*increase the size of the array*/
+
 	ret = realloc(ret, (i + 1) * sizeof(char *));
 	if (!ret)
 		return (NULL);
@@ -39,12 +30,6 @@ char **tokenize(char *str, const char *delim)
 	return (ret);
 }
 
-/**
- * tokenize_input - splits a user input string into tokens with tokenize().
- * @input: the user input string to be tokenized
- *
- * Return: an array of pointers to the tokens, or NULL if an error occurs
- */
 char **tokenize_input(char *input)
 {
 	char **tokens = NULL;
