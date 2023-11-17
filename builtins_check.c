f7cf0f0c3f2833255cf4c547b67d084b2568b569
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-11-17 10:13:11 +0200

    Update builtins_check.c

--- builtins_check.c
+++ builtins_check.c
@@ -1,11 +1,5 @@
  #include "shell.h"
 
-/**
- * check_for_builtin - checks if the command is a builtin
- * @args: the arguments array
- *
- * Return: 1 if command is a builtin, 0 otherwise
- */
 int check_for_builtin(char **args)
 {
 	if (!args[0])
